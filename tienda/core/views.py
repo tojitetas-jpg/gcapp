ea64e7e28899c56c3e05bc4728ceed3f8895937e
Alma <almita.wagner@gmail.com>
2026-05-05 08:20:52 +0200

    productos dinamicos, secciones productos populares y mejores precios, pagina template producto

--- tienda/core/views.py
+++ tienda/core/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from productos.models import Product 
 
 def home(request):
-    return render(request, 'home.html')
+    popular_products = Product.objects.filter(popular=True)[:3]
+    best_price_products = Product.objects.filter(best_price=True)[:3]
+    return render(request, 'home.html', {
 
+        'popular_products': popular_products,
+        'best_price_products': best_price_products,
+
+    })
+   
 def about(request):
     return render(request, 'about.html')
 
--- tienda/productos/models.py
+++ tienda/productos/models.py
@@ -5,6 +5,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.URLField(blank=True, null=True)
+    best_price = models.BooleanField(default=False)
+    popular = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
