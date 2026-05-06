9c57950924b3ee41fd9848ad03ace052092828ca
Alma <almita.wagner@gmail.com>
2026-05-06 11:52:41 +0200

    Categorias y arreglos de estilo

--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#imagenes
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- tienda/tienda/urls.py
+++ tienda/tienda/urls.py
@@ -1,11 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
-
-#views de cada app
-
+# views de cada app
 from core import views as core_views
 from usuarios import views as user_views
 from productos import views as product_views
@@ -13,24 +12,25 @@ from pedidos import views as order_views
 
 urlpatterns = [
 
-    #Admin
+    # Admin
     path('admin/', admin.site.urls),
 
-    #Paginas principales (core)
+    # Paginas principales (core)
     path("", core_views.home, name="home"),
     path("contact/", core_views.contact, name="contact"),
     path("about/", core_views.about, name="about"),
     path("services/", core_views.services, name="services"),
 
-    #Productos
+    # Productos
     path("all-products/", product_views.products, name="products"),
     path("product/<int:id>/", product_views.product, name="product"),
     path('categorias/<slug:slug>/', product_views.category_products, name='category_products'),
 
-    #Usuarios
+    # Usuarios
     path("login/", user_views.login_view, name="login"),
     path("register/", user_views.register_view, name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
-
 ]
 
+# imagenes categorias
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
