c3a82492fe882805210483d0f7ed433bb219a0b3
Alma <almita.wagner@gmail.com>
2026-05-11 20:27:33 +0200

    main

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\l2178\Documents\jing\gcapp\venv
+set VIRTUAL_ENV=C:\Users\Alma_\OneDrive\Documentos\FP\Proyecto_GCapp\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
